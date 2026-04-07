05a298a8df32f2e06de2bf77a064f863e4b01c3c
pashupati-git <pashupatic.github@gmail.com>
2026-04-07 14:38:33 +0545

    rc no reason screen completed

---
+++ lib/features/rc_rollback/screens/rc_rollback_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RcRollbackScreen extends StatelessWidget {
+  const RcRollbackScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Rollback Screen.')),
+    );
+  }
+}
