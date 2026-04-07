15c7fe3909065f88b02bfe14cd90c14c3ff86390
pashupati-git <pashupatic.github@gmail.com>
2026-04-07 11:23:14 +0545

    work started at rc no reason scren

--- lib/features/rc_rollback/screens/rc_no_reason_screen.dart
+++ lib/features/rc_rollback/screens/rc_no_reason_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-class RcNoReasonScreen extends StatelessWidget {
+class RcNoReasonScreen extends StatefulWidget {
   const RcNoReasonScreen({super.key});
+
+  @override
+  State<RcNoReasonScreen> createState() => _RcNoReasonScreenState();
+}
+
+class _RcNoReasonScreenState extends State<RcNoReasonScreen> {
+  final _notesController = TextEditingController();
+  final _remarksController=TextEditingController();
+  DateTime? _revisitDate;
+
+
+  //toggle states - wire to Bloc/Provider later
+  final Map<String,bool> _toggles={
+    'offer':false,
+    'escalate':false,
+    'partial':false,
+    'churned':false,
+  };
 
   @override
   Widget build(BuildContext context) {
