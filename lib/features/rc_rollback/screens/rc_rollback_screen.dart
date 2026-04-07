b0a62985c855dfd2f26b53079d38aee2b00a92cd
pashupati-git <pashupatic.github@gmail.com>
2026-04-07 15:11:04 +0545

    started work at rollback_screen

--- lib/features/rc_rollback/screens/rc_rollback_screen.dart
+++ lib/features/rc_rollback/screens/rc_rollback_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
-class RcRollbackScreen extends StatelessWidget {
+class RcRollbackScreen extends StatefulWidget {
   const RcRollbackScreen({super.key});
 
+  @override
+  State<RcRollbackScreen> createState() => _RcRollbackScreenState();
+}
+
+class _RcRollbackScreenState extends State<RcRollbackScreen> {
+  String? _uninstallReason;
+  String? _detailReason;
+  String? _nextIsp;
+
+
+  final _onuSerialCtrl=TextEditingController(text:'NBELFBBA2FE2');
+  final _onuQtyCtrl=TextEditingController(text:'1');
+  final _depositCtrl=TextEditingController(text:'500');
+  final _wire100Ctrl=TextEditingController(text:'100');
+  final _wire150Ctrl=TextEditingController(text:'100');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
